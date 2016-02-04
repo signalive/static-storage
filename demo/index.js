020211e70e32a26dc8502b79ad312b0a0d5d66c2
Mehmet Tamturk <mehmettamturk@gmail.com>
2016-02-05 01:49:07 +0200

    chore(s3): Add example methods to demo script.

--- demo/index.js
+++ demo/index.js
@@ -17,4 +17,8 @@ const StaticStorage = require('../src')({
     }
 });
 
-StaticStorage.uploadToTmp('../demo/demo.txt', 'tempFileName.txt');
+StaticStorage.uploadToTmp('../demo/demo.txt', 'tempFileName.txt');     // Upload file
+//StaticStorage.readObject('tmp/tempFileName.txt');                    // Read file
+//StaticStorage.move('tmp/tempFileName.txt', 'copy/tempFileName.txt'); // Move file
+//StaticStorage.remove('copy/tempFileName.txt');                       // Remove file
+//StaticStorage.remove('copy');                                        // Removes folder
