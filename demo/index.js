cbe2642d9c098b772150aa097839ef39878c1eff
batilc <batilc@batilcs-MacBook-Pro.local>
2022-07-05 11:10:19 +0100

    :sparkles: Implement minio

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const util = require('util');
+
 const StaticStorage = require('../src')({
     // 'staticstorage': {
     //     "strategy": "azurestorage",
@@ -19,8 +21,9 @@ const StaticStorage = require('../src')({
     //   "tmpFolderPath": "/tmp"
     // },
     staticstorage: {
-      strategy: 'bluemix',
-      tmpFolderPath: '/tmp'
+      strategy: 'minio',
+      tmpFolderPath: '/tmp',
+      rootPath: '/public'
     },
     bluemix: {
       bucketName: '<bucket_name>',
@@ -48,60 +51,90 @@ const StaticStorage = require('../src')({
     'gcloud': {
         "projectId": "<project_id>",
         "bucketName": "<bucket_name>"
+    },
+    'minio': {
+      "endPoint": "localhost",
+      "port": "9000",
+      "bucketName": "static",
+      "accesskeyid": 'KT5KRrCqk62KSHu8',
+      "secretaccesskey": 'sMelEcokbZcYbRWRjLKasMSO8j5qkFPt',
     }
 });
 
-// azure storage
-// StaticStorage.upload('demo/demo.txt', 'tat/tempFileName.txt');            // Upload file
-// StaticStorage.uploadToTmp('demo/demo.txt', 'tempFileName.txt');           // Upload file
-// StaticStorage.downloadToLocalTmp('tmp/tempFileName.txt');                 // Read file
-// StaticStorage.copy('tmp/tempFileName.txt', 'copy/tempFileName.txt');      // Move file
-// StaticStorage.remove('copy/tempFileName.txt');                            // Remove file
-// StaticStorage.remove('copy');
-// StaticStorage.listFiles().then(response => console.log(response));
-// StaticStorage.getFileSizeInBytes('hayret/festivest/2a1995449069b71b116b72b3061fbe14.jpg').then(response => console.log(response))
-// StaticStorage.removeFolder('memo').then(response => console.log(response));
-// StaticStorage.copy('hayret/festivest/2a1995449069b71b116b72b3061fbe14.jpg', 'memo/deneme.jpg').then(response => console.log(response));
+async function run() {
+  // azure storage
+  // await StaticStorage.upload('demo/demo.txt', 'tat/tempFileName.txt');            // Upload file
+  // await StaticStorage.uploadToTmp('demo/demo.txt', 'tempFileName.txt');           // Upload file
+  // await StaticStorage.downloadToLocalTmp('tmp/tempFileName.txt');                 // Read file
+  // await StaticStorage.copy('tmp/tempFileName.txt', 'copy/tempFileName.txt');      // Move file
+  // await StaticStorage.remove('copy/tempFileName.txt');                            // Remove file
+  // await StaticStorage.remove('copy');
+  // await StaticStorage.listFiles().then(response => console.log(response));
+  // await StaticStorage.getFileSizeInBytes('hayret/festivest/2a1995449069b71b116b72b3061fbe14.jpg').then(response => console.log(response))
+  // await StaticStorage.removeFolder('memo').then(response => console.log(response));
+  // await StaticStorage.copy('hayret/festivest/2a1995449069b71b116b72b3061fbe14.jpg', 'memo/deneme.jpg').then(response => console.log(response));
 
-// s3
-// StaticStorage.uploadToTmp('../demo/demo.txt', 'tempFileName.txt');        // Upload file
-//StaticStorage.downloadToLocalTmp('tmp/tempFileName.txt');                  // Read file
-//StaticStorage.move('tmp/tempFileName.txt', 'copy/tempFileName.txt');       // Move file
-//StaticStorage.remove('copy/tempFileName.txt');                             // Remove file
-//StaticStorage.remove('copy');
+  // s3
+  // await StaticStorage.uploadToTmp('../demo/demo.txt', 'tempFileName.txt');        // Upload file
+  //await StaticStorage.downloadToLocalTmp('tmp/tempFileName.txt');                  // Read file
+  //await StaticStorage.move('tmp/tempFileName.txt', 'copy/tempFileName.txt');       // Move file
+  //await StaticStorage.remove('copy/tempFileName.txt');                             // Remove file
+  //await StaticStorage.remove('copy');
 
-// StaticStorage.uploadToTmp('../demo/demo.txt', 'tempFileName.txt');        // Upload file
-// StaticStorage.downloadToLocalTmp('tmp/tempFileName.txt');                 // Read file
-// StaticStorage.move('tmp/tempFileName.txt', 'copy/tempFileName.txt');      // Move file
-// StaticStorage.remove('copy/tempFileName.txt');                            // Remove file
-// StaticStorage.remove('copy');
+  // await StaticStorage.uploadToTmp('../demo/demo.txt', 'tempFileName.txt');        // Upload file
+  // await StaticStorage.downloadToLocalTmp('tmp/tempFileName.txt');                 // Read file
+  // await StaticStorage.move('tmp/tempFileName.txt', 'copy/tempFileName.txt');      // Move file
+  // await StaticStorage.remove('copy/tempFileName.txt');                            // Remove file
+  // await StaticStorage.remove('copy');
 
-// Google Cloud
-// StaticStorage.move('demo.txt', 'tmp/demo2.txt');
-// StaticStorage.copy('demo.txt', 'demo2.txt');
-// StaticStorage.remove('demo.txt');
-// StaticStorage.upload('demo.txt', 'test/demo.txt');
-// StaticStorage.upload('demo.txt', 'demo.txt');
-// StaticStorage.uploadToTmp('demo.txt', 'demo-2.txt');
-// StaticStorage.removeFolder('tmp');
-// StaticStorage.downloadToLocalTmp('demo2.txt');
-// StaticStorage.getFileSizeInBytes('demo2.txt').then(a => console.log('a', a));
+  // Google Cloud
+  // await StaticStorage.move('demo.txt', 'tmp/demo2.txt');
+  // await StaticStorage.copy('demo.txt', 'demo2.txt');
+  // await StaticStorage.remove('demo.txt');
+  // await StaticStorage.upload('demo.txt', 'test/demo.txt');
+  // await StaticStorage.upload('demo.txt', 'demo.txt');
+  // await StaticStorage.uploadToTmp('demo.txt', 'demo-2.txt');
+  // await StaticStorage.removeFolder('tmp');
+  // await StaticStorage.downloadToLocalTmp('demo2.txt');
+  // await StaticStorage.getFileSizeInBytes('demo2.txt').then(a => console.log('a', a));
 
-// Bluemix
-// StaticStorage.move('demo.txt', 'tmp/demo2.txt');
-// StaticStorage.copy('demo.txt', 'demo2.txt');
-// StaticStorage.remove('demo.txt');
-// StaticStorage.upload('./demo.txt', 'test/demo.txt');
-// StaticStorage.upload('demo.txt', 'demo.txt');
-// StaticStorage.uploadToTmp('demo.txt', 'demo-2.txt');
-// StaticStorage.removeFolder('test');
-// StaticStorage.downloadToLocalTmp('demo2.txt');
-// StaticStorage.getFileSizeInBytes('demo2.txt').then(a => console.log('a', a));
+  // Bluemix
+  // await StaticStorage.move('demo.txt', 'tmp/demo2.txt');
+  // await StaticStorage.copy('demo.txt', 'demo2.txt');
+  // await StaticStorage.remove('demo.txt');
+  // await StaticStorage.upload('./demo.txt', 'test/demo.txt');
+  // await StaticStorage.upload('demo.txt', 'demo.txt');
+  // await StaticStorage.uploadToTmp('demo.txt', 'demo-2.txt');
+  // await StaticStorage.removeFolder('test');
+  // await StaticStorage.downloadToLocalTmp('demo2.txt');
+  // await StaticStorage.getFileSizeInBytes('demo2.txt').then(a => console.log('a', a));
 
 
-// Local
-//StaticStorage.upload('demo/demo.txt', 'memo/tempFileName.txt');            // Upload file
-//StaticStorage.downloadToLocalTmp('/tmp/tempFileName.txt');                 // Read file
-//StaticStorage.move('/tmp/tempFileName.txt', 'copy/tempFileName.txt');      // Move file
-//StaticStorage.remove('../preserved/tempFileName.txt');                     // Remove file
-//StaticStorage.remove('../preserved');                                      // Remove folder
+  // Minio
+  await StaticStorage.listFiles().then(r => console.log(util.inspect(r.map(({Key}) => Key), {depth: 5, colors: true})));
+  await StaticStorage.upload('demo.txt', 'demo.txt');
+  await StaticStorage.copy('demo.txt', 'demo2.txt');
+  await StaticStorage.move('demo.txt', 'tmp/demo2.txt');
+  await StaticStorage.upload('./demo.txt', 'test/demo.txt');
+  await StaticStorage.uploadToTmp('demo.txt', 'demo-2.txt');
+  await StaticStorage.listFiles().then(r => console.log(util.inspect(r.map(({Key}) => Key), {depth: 5, colors: true})));
+  await StaticStorage.removeFolder('test');
+  await StaticStorage.downloadToLocalTmp('demo2.txt');
+  await StaticStorage.downloadToLocalTmp('tmp/demo2.txt');
+  await StaticStorage.getFileSizeInBytes('demo2.txt').then(size => console.log('Size is', size));
+  await StaticStorage.remove('demo2.txt');
+  await StaticStorage.remove('tmp/demo2.txt');
+  await StaticStorage.remove('tmp/demo-2.txt');
+  await StaticStorage.listFiles().then(r => console.log(util.inspect(r.map(({Key}) => Key), {depth: 5, colors: true})));
+
+  // Local
+  //await StaticStorage.upload('demo/demo.txt', 'memo/tempFileName.txt');            // Upload file
+  //await StaticStorage.downloadToLocalTmp('/tmp/tempFileName.txt');                 // Read file
+  //await StaticStorage.move('/tmp/tempFileName.txt', 'copy/tempFileName.txt');      // Move file
+  //await StaticStorage.remove('../preserved/tempFileName.txt');                     // Remove file
+  //await StaticStorage.remove('../preserved');                                      // Remove folder
+
+  console.log('Demo test is completed with success');
+}
+
+run();
---
+++ src/minio.js
@@ -0,0 +1,181 @@
+const minio = require('minio')
+const path = require('path');
+const mkdirp = require('mkdirp-then');
+const debug = require('debug')('static-storage:gcloud');
+const MimeDetector = require('mime-types');
+MimeDetector.types.gz = 'application/x-gzip';
+
+class Minio {
+    constructor(configParams) {
+        this.config = configParams;
+        this.tmpFolderPath = this.config.staticstorage.tmpFolderPath.slice(1);
+        this.bucketName = this.config.minio.bucketName;
+
+        this.minio =  new minio.Client({
+            endPoint: this.config.minio.endPoint,
+            port: parseInt(this.config.minio.port, 10),
+            accessKey: this.config.minio.accessKeyId,
+            secretKey: this.config.minio.secretAccessKey,
+            useSSL: false
+        });
+
+        this.createBucketIfNotExists();
+    }
+
+
+    /**
+     * Create bucket on initialization.
+     * @return {Promise}
+     */
+     async createBucketIfNotExists() {
+        debug(`Checking if bucket exists: ${this.bucketName}`);
+
+        const exists = await this.minio.bucketExists(this.bucketName);
+        if (!exists) {
+            console.log(`Bucket ${this.bucketName} does not exist, will create one`);
+            await this.minio.makeBucket(this.bucketName, 'eu-west-1');
+        }
+    }
+
+
+    /**
+     * Adds slash if str params not has on first.
+     * @param {String} str
+     * @returns {string}
+     * @private
+     */
+     addSlash_(str) {
+        if (!str || str[0] == '/') return str;
+        return '/' + str;
+    }
+
+
+    /**
+     * Uploads a file from local fs to the s3 bucket.
+     * @param {string} src
+     * @param {string} dst
+     * @returns {Promise}
+     */
+    async upload(src, dst) {
+        debug(`Uploading from local ${src} to bucket ${this.bucketName}.`);
+
+        const stats = await this.minio.fPutObject(this.bucketName, dst, src, {
+            'Content-Type': MimeDetector.contentType(path.extname(src)) || 'application/octet-stream'
+        });
+
+        return stats;
+    }
+
+
+    /**
+     * Uploads a file from local fs to the tmp folder in the s3 bucket.
+     * @param {string} src
+     * @param {string} dst
+     * @returns {Promise}
+     */
+    uploadToTmp(src, dst) {
+        return this.upload(src, path.join(this.tmpFolderPath, dst));
+    }
+
+
+    /**
+     * Generic list method to list files of current s3 bucket.
+     * @returns {Promise}
+     */
+    listFiles(prefix = '') {
+        return new Promise((resolve, reject) => {
+            const list = [];
+            const stream = this.minio.listObjects(this.bucketName, prefix, true);
+
+            stream.on('error', function(err) {
+                console.log('Error while listing files', err);
+                reject(err);
+            });
+
+            stream.on('data', function(obj) {
+                list.push({Key: obj.name});
+            });
+
+            stream.on('end', function (obj) {
+                resolve(list);
+            });
+        });
+    }
+
+
+    /**
+     * Generic download method.
+     * @param {string} downloadPath
+     * @returns {Promise}
+     */
+    async downloadToLocalTmp(downloadPath) {
+        await mkdirp(path.join('./', this.tmpFolderPath));
+
+        const tmpFileName = path.join('./', this.tmpFolderPath,
+            new Date().getTime() + downloadPath.slice(downloadPath.lastIndexOf('.')));
+        await this.minio.fGetObject(this.bucketName, downloadPath, tmpFileName);
+
+        return tmpFileName;
+    }
+
+
+    /**
+     * Removes an existing file.
+     * @param {string} path
+     * @returns {Promise}
+     */
+    async remove(path) {
+        debug(`Removing ${path} from bucket ${this.bucketName}.`);
+        await this.minio.removeObject(this.bucketName, path);
+    }
+
+
+    /**
+     * Removes entire folder and its subfolders
+     * @param  {string} folderPath
+     * @return {Promise}
+     */
+    async removeFolder(folderPath) {
+        debug(`Removing folder at ${folderPath}`);
+        const files = await this.listFiles(folderPath);
+        await this.minio.removeObjects(this.bucketName, files.map(({Key}) => Key));
+    }
+
+
+    /**
+     * Copies an existing file to a new location.
+     * @param {string} src
+     * @param {string} dst
+     * @returns {Promise}
+     */
+    async copy(src, dst) {
+        debug(`Copying file ${src} to ${dst}.`);
+        await this.minio.copyObject(this.bucketName, dst, path.join(this.bucketName, src));
+    }
+
+
+    /**
+     * Moves file.
+     * @param {string} src Path of source file.
+     * @param {string} dst Path of distance file.
+     * @returns {Promise}
+     */
+    async move(src, dst) {
+        await this.copy(src, dst);
+        await this.remove(src);
+    }
+
+
+    async stats_(src) {
+        const stats = await this.minio.statObject(this.bucketName, src);
+        return stats;
+    }
+
+
+    async getFileSizeInBytes(src) {
+        const metadata = await this.stats_(src);
+        return parseInt(metadata.size, 10);
+    }
+}
+
+module.exports = Minio;
